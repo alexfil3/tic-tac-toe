b77c4cea8b15d9bd8d951ee5da45dd34f020fb8c
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-05-16 23:08:30 +0300

    complete the game

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
-
-export default function Square({ value }) {
-  return <button className="square">{value}</button>;
+export default function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
